9b94d07c4261324f0fb52bf1f9c70bde28353d8f
František Moucha <fand.mou@gmail.com>
2024-03-29 21:58:34 -0700

    opravy users page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -9,7 +9,7 @@
       />
     </div>
 
-    <n-button @click="showAddUserModal">Vytvořit nabídku</n-button>
+    <n-button @click="showAddUserModal">Přidat uživatele</n-button>
 
     <n-data-table :columns="columns" :data="filteredUsers" class="users-table">
       <template v-slot:action="{ row }">
@@ -28,18 +28,32 @@
       <template #body>
         <div v-if="showAddModal">
           <n-form @submit.prevent="addUser">
-            <n-form-item label="User Name">
-              <n-input v-model="newUser.name" placeholder="Enter User name" />
+            <n-form-item label="E-mail">
+              <n-input v-model="newUser.email" placeholder="Enter user email" />
             </n-form-item>
-            <n-form-item label="Address">
-              <n-input v-model="newUser.address" placeholder="Enter address" />
-            </n-form-item>
-            <n-form-item label="Contact Person">
+            <n-form-item label="Password">
               <n-input
-                v-model="newUser.contact"
-                placeholder="Enter contact person"
+                v-model="newUser.password"
+                placeholder="Enter password"
+                type="password"
               />
             </n-form-item>
+            <n-form-item label="First Name">
+              <n-input
+                v-model="newUser.firstName"
+                placeholder="Enter first name"
+              />
+            </n-form-item>
+            <n-form-item label="Last Name">
+              <n-input
+                v-model="newUser.lastName"
+                placeholder="Enter last name"
+              />
+            </n-form-item>
+            <n-form-item label="Right ID">
+              <n-input v-model="newUser.rightId" placeholder="Enter right ID" />
+            </n-form-item>
+            <n-button type="submit" class="modal-add-button">Vytvořit Uživatele</n-button>
           </n-form>
         </div>
         <div v-if="showDetailModal">
@@ -67,11 +81,11 @@
         </div>
       </template>
       <template #footer>
-        <n-button @click="closeModal" class="modal-close-button"
-          >Close</n-button
+        <n-button @click="hideAddUserModal" class="modal-close-button"
+          >Zařít</n-button
         >
         <n-button v-if="showAddModal" @click="addUser" class="modal-add-button"
-          >Add User</n-button
+          >Vtyvořit Uživatele</n-button
         >
       </template>
     </custom-modal>
@@ -101,21 +115,67 @@ export default {
     const modalTitle = ref("");
     const showAddModal = ref(false);
     const showDetailModal = ref(false);
-    const newUser = ref({ name: "", address: "", contact: "" });
     const selectedUserDetails = ref({});
     const filters = ref({ searchQuery: "" });
+    const newUser = ref({
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      rightId: "",
+    });
+
+    // Function to show modal for adding a new user
+    const showAddUserModal = () => {
+      isModalVisible.value = true;
+      modalTitle.value = "Add New User";
+      showAddModal.value = true;
+    };
+
+    // Function to hide modal for adding a new user
+    const hideAddUserModal = () => {
+      isModalVisible.value = false;
+      modalTitle.value = "Add New User";
+      showAddModal.value = false;
+    };
+
+    // Function to add a new user
+    const addUser = async () => {
+      try {
+        await userApi.createUser({ ...newUser.value });
+        // Clear form
+        newUser.value = {
+          email: "",
+          password: "",
+          firstName: "",
+          lastName: "",
+          rightId: "",
+        };
+        isModalVisible.value = false;
+        showAddModal.value = false;
+        // Optionally, refresh the list of users
+      } catch (error) {
+        console.error("Failed to add user:", error);
+      }
+    };
+
 
     const loadUsers = async () => {
       try {
-        const response = await userApi.userUsersAndUserCreationDataGet();
-        if (
-          response.data &&
-          response.data.result &&
-          Array.isArray(response.data.result.users)
-        ) {
-          users.value = response.data.result.users; // Use the users array from the response
-        } else {
-          console.error("Unexpected response format:", response.data);
+        const token = localStorage.getItem("authToken");
+        if (token) {
+          const response = await userApi.userUsersAndUserCreationDataGet({
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          if (
+            response.data &&
+            response.data.result &&
+            Array.isArray(response.data.result.users)
+          ) {
+            users.value = response.data.result.users; // Use the users array from the response
+          } else {
+            console.error("Unexpected response format:", response.data);
+          }
         }
       } catch (error) {
         console.error("Failed to load users:", error);
@@ -144,13 +204,23 @@ export default {
     });
 
     const columns = ref([
-      { title: "Email", key: "email" },
-      { title: "First Name", key: "firstName" },
-      { title: "Last Name", key: "lastName" },
       {
-        title: "Organization",
+        title: "No",
+        key: "no",
+        render: (row) => filteredUsers.value.indexOf(row) + 1,
+      },
+      { title: "E-mail", key: "email" },
+      { title: "Jméno", key: "firstName" },
+      { title: "Příjmení", key: "lastName" },
+      {
+        title: "Organizace",
         key: "organizationName",
         render: (row) => row.organization.name,
+      },
+      {
+        title: "Oprávění",
+        key: "right",
+        render: (row) => row.right.name,
       },
       {
         title: "Detail",
@@ -158,7 +228,6 @@ export default {
         render: (row) =>
           h(NButton, { onClick: () => prepareUserDetails(row) }, "Detail"),
       },
-      // Define other columns as needed
     ]);
 
     return {
@@ -167,15 +236,10 @@ export default {
       filteredUsers,
       selectedUserDetails,
       isModalVisible,
-      modalTitle,
-      itemColumns,
-      showAddModal,
-      showDetailModal,
-      newUser,
       showAddUserModal,
-      addUser,
-      closeModal,
-      prepareUserDetails,
+      hideAddUserModal,
+      modalTitle,
+      newUser,
       columns,
     };
   },
