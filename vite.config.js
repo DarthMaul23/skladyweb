ffd282666266178b8415dbb2c3f96982f1f80717
František Moucha <fand.mou@gmail.com>
2024-04-30 23:57:59 -0700

    oprava .env

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // Using the environment variable or a default value
-const API_URI = process.env.VITE_BASE_API_URL || 'https://localhost:7292/';
+const API_URI = process.env.VITE_BASE_API_URL || 'https://template-test-api.azurewebsites.net/';
 
 export default defineConfig({
   plugins: [vue()],
